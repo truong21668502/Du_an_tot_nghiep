d4af154f48d4530c068d409b0c5e1884ab9deca1
truong21668502 <truong21668502@gmail.com>
2026-06-09 16:07:26 +0700

    Giao diện đăng ký đăng nhập + chuẩn bị sẵn js

--- cafe-project/app/Models/User.php
+++ cafe-project/app/Models/User.php
@@ -19,7 +19,7 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
+        'full_name',
         'email',
         'password',
     ];
