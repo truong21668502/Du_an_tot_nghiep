24b597f1f061cde148e2e7777874a1a4f166492a
truong21668502 <truong21668502@gmail.com>
2026-06-22 19:05:05 +0700

    fix ip v4

--- cafe-project/vite.config.js
+++ cafe-project/vite.config.js
@@ -4,13 +4,13 @@ import vue from '@vitejs/plugin-vue';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-        server: {
-        host: '0.0.0.0',
-        cors: true,
-        hmr: {
-            host: '192.168.12.15',
-        },
-    },
+    //     server: {
+    //     host: '0.0.0.0',
+    //     cors: true,
+    //     hmr: {
+    //         host: '192.168.12.15',
+    //     },
+    // },
     plugins: [
         laravel({
             input: 'resources/css/app.css',
